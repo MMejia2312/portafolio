52f10e9018d816719e32d3b6b77ac9cb34f5ff46
MMejia2312 <98856050+MMejia2312@users.noreply.github.com>
2022-03-27 20:54:46 -0600

    Add files via upload

---
+++ portafolio_actualizacionHerencias/Enemigos.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.portafolio;
+
+/**
+ *
+ * @author mario mejía
+ */
+/*la clase Enemigos es una clase hija, la cual hereda los datos generales de la clase ya existente personajes
+ademas de añadig algunos datos extra que son necesario para el juego.*/
+public class Enemigos extends personajes {
+     private int dificultad;
+     
+   
+    public void setDificculdad (int _dificultad)
+    {
+        this.dificultad=_dificultad;
+    }
+    
+    public int getDificultad(){
+     return this.dificultad;
+    }
+     public void muestraDatos(){
+         System.out.println("enemigo: " + nombre);
+         System.out.println("Vida: " + getVida()); 
+        /*se hace llamada al metodo constructor que nos permite revisar la vida del personaje enemigo
+         esto con el fin de poder mostrar los datos requeridos sin necesidad de alterar en encapsulamiento
+         de la clase personajes*/
+        System.out.println("Con un poder de: " + getPoder());
+        System.out.println("Este tiene un grado de dificultad de : " + getDificultad());
+        
+     }
+     
+     public void crearEnemigo (String _nombre,int _vida, int _poder, int _vAtaque,int _dificultad){
+     
+        nombre =_nombre;
+        vida = _vida;
+        poder =_poder;
+        vAtaque =_vAtaque;
+        dificultad=_dificultad;
+     }
+}
